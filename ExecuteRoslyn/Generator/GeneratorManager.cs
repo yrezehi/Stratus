12125cfee6c8a804019fc0efd22b453ea2ce7917
unknown <yrezehi@gmail.com>
2023-09-19 06:02:50 -0700

    add: base folder

---
+++ ExecuteRoslyn/Generator/GeneratorManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecuteRoslyn.Generator
+{
+    public class GeneratorManager
+    {
+        public GeneratorManager() { }
+    }
+}
