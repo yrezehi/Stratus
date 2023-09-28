67a5496d3ead844c50459f70811873260d54d292
unknown <yrezehi@gmail.com>
2023-09-28 05:44:37 -0700

    add: dom.js

---
+++ Static/wwwroot/js/dom.js
@@ -0,0 +1,16 @@
+﻿var dom = function () {
+
+    function createElementFromSerlized(serlizedElement) {
+        var temporaryElement = document.createElement("div");
+
+        temporaryElement.innerHTML += serlizedElement;
+
+        return temporaryElement.firstElementChild;
+    }
+
+    return function () {
+        return Object.freeze({
+            createElement: createElementFromSerlized
+        });
+    }();
+}();
