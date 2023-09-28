d9608cea90675aed4f2bda59763d042fda659a05
unknown <yrezehi@gmail.com>
2023-09-28 05:45:40 -0700

    add: global.js

---
+++ Static/wwwroot/js/globals.js
@@ -0,0 +1,25 @@
+﻿var globalProperties = function () {
+
+    var properties = {};
+
+    function setProperty(key, value) {
+        properties = { ...properties, [key]: value };
+    }
+
+    function getProperty(key) {
+
+        if (!properties.hasOwnProperty(key)) {
+            notification.error(`global property named ${key} can't be found!`);
+            return;
+        }
+
+        return properties[key];
+    }
+
+    return function () {
+        return {
+            set: setProperty,
+            get: getProperty,
+        };
+    }();
+}();
