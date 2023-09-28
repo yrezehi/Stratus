6dbccd22c4236d896e3cd09a9aa23eb466c6cbae
unknown <yrezehi@gmail.com>
2023-09-28 05:26:16 -0700

    clean: static project #3

---
+++ Static/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Static.Models;
+using Static.Models.ViewModels;
+using System.Diagnostics;
+
+namespace Static.Controllers
+{
+    [Route("[controller]")]
+    public class ErrorController : Controller
+    {
+        public ErrorController() {  }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() => 
+            View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
--- Static/Controllers/HomeController.cs
+++ Static/Controllers/HomeController.cs
@@ -1,27 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Static.Models;
+using Static.Models.ViewModels;
 using System.Diagnostics;
 
 namespace Static.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
