30df3cab2ab851b4e42be1af3bbf391e4276c04f
unknown <yrezehi@gmail.com>
2023-09-28 06:57:17 -0700

    add: MapPathToHTML

--- UI/HTML/HTMLResult.cs
+++ UI/HTML/HTMLResult.cs
@@ -1,10 +1,28 @@
-﻿namespace UI.HTML
+﻿using System.Net.Mime;
+using System.Text;
+using System.Text.Unicode;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace UI.HTML
 {
     public class HTMLResult : IResult
     {
+
+        private static string DEFAULT_INDEX_PAGE_NAME = "index";
+
         public Task ExecuteAsync(HttpContext httpContext)
         {
+            httpContext.Response.ContentType = Text.Html;
+            httpContext.Response.ContentLength = Encoding.UTF8.GetByteCount(MapPathToHTML(httpContext.Request.Path));
+
             throw new NotImplementedException();
+        }
+
+        private string MapPathToHTML(string requestPath)
+        {
+            requestPath = string.IsNullOrEmpty(requestPath) ? DEFAULT_INDEX_PAGE_NAME : requestPath;
+
+            return requestPath;
         }
     }
 }
