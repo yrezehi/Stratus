0ead3f475e74a8d0b690aed3571979358f36e177
unknown <yrezehi@gmail.com>
2023-09-29 10:24:33 -0700

    rm: swagger

--- UI/Program.cs
+++ UI/Program.cs
@@ -2,19 +2,7 @@ using UI;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.RegisterControllers();
 
