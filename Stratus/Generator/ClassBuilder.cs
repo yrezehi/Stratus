8c4fb56fdf30d820e1264b6b1974499bc10af0c2
unknown <yrezehi@gmail.com>
2023-09-29 20:50:21 -0700

    setup: class builder

---
+++ Stratus/Generator/ClassBuilder.cs
@@ -0,0 +1,31 @@
+﻿using Stratus.Declarations.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stratus.Generator
+{
+    public class ClassBuilder
+    {
+        protected Class Class;
+
+        private ClassBuilder() { 
+            
+            Class = new Class(); 
+        }
+
+        public static ClassBuilder Builder() => new ClassBuilder();
+
+        public ClassBuilder WithName(string name)
+        {
+            Class.Name = name;
+            return this;
+        }
+
+        public Class Build() => Class;
+    }
+}
--- Stratus/Program.cs
+++ Stratus/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Stratus.Declarations.Dynamic.Entities;
+using Stratus.Generator;
 
-Console.WriteLine(Entity.Build("Pizza"));
+Console.WriteLine("Finished Execution!");
