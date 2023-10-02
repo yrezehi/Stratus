84a8aab256a25f64d9181a96614b76991bedfb64
unknown <yrezehi@gmail.com>
2023-10-02 12:51:40 -0700

    setup: few functions for generator

--- Stratus/Generator.cs
+++ Stratus/Generator.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Stratus
 {
     public class Generator
     {
-        public void GlobalVariable(FieldDeclarationSyntax variable)
-        {
+        public SyntaxNode GlobalVariable(ClassDeclarationSyntax @class, FieldDeclarationSyntax variable) =>
+           @class.AddMembers(variable);
 
-        }
+        public Document? UpdateClass(Project project, SyntaxTree sourceTree, SyntaxNode @class) =>
+            project.GetDocument(sourceTree)?.WithSyntaxRoot(@class);
+
+        public void UpdateSolution(Workspace workspace, Document document) =>
+            workspace.TryApplyChanges(document.Project.Solution);
     }
 }
