03ae97fef8490ea4d5ee395f45e5257381b13752
unknown <yrezehi@gmail.com>
2023-10-02 12:31:15 -0700

    add: get project and class

---
+++ Stratus/Exceptions/WorkspaceFailures.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stratus.Exceptions
+{
+    public static class WorkspaceFailures
+    {
+        public static void OnWorkspaceFailed(object? sender, WorkspaceDiagnosticEventArgs @event)
+        {
+            Console.WriteLine(@event.Diagnostic.Message);
+        }
+    }
+}
--- Stratus/Loader.cs
+++ Stratus/Loader.cs
@@ -2,6 +2,7 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.MSBuild;
+using Stratus.Exceptions;
 
 namespace Stratus
 {
@@ -13,26 +14,36 @@ namespace Stratus
         public static async Task<Loader> Load(string path) {
             MSBuildLocator.RegisterDefaults();
             Loader loader = new Loader();
+
             loader.Workspace = MSBuildWorkspace.Create();
             loader.Solution = await loader.Workspace.OpenSolutionAsync(path);
+
+            loader.Workspace.WorkspaceFailed += WorkspaceFailures.OnWorkspaceFailed;
+
             return loader;
         }
 
-        public async void Inquery()
+        public Project? GetProject(string name) =>
+            Solution.Projects.FirstOrDefault(project => project.Name == name); 
+
+        public async INamedTypeSymbol GetClass(string projectName, string className)
         {
-            foreach (var project in Solution.Projects.SelectMany(csProject => csProject.Documents))
-            {
-                var syntaxRoot = await project.GetSyntaxRootAsync();
-                var semanticModel = await project.GetSemanticModelAsync();
-                if(syntaxRoot != null && semanticModel != null) {
-                    var declarations = syntaxRoot.DescendantNodes().OfType<LocalDeclarationStatementSyntax>();
-                    foreach (var declaration in declarations)
-                    {
-                        var symbolInformation = semanticModel.GetSymbolInfo(declaration);
-                        var typeSymbol = symbolInformation.Symbol;
-                    }
-                }
-            }
+            Project? project = this.GetProject(projectName);
+
+            if (project == null)
+                throw new Exception($"Couldn't load project {projectName}");
+
+            Compilation? compilation = await project.GetCompilationAsync();
+
+            if (compilation == null)
+                throw new Exception($"Couldn't load compilation for {projectName}");
+
+            INamedTypeSymbol? classTypeSymbol = compilation.GetTypeByMetadataName(className);
+
+            if (classTypeSymbol == null)
+                throw new Exception($"Couldn't load named type symbol for {projectName}");
+
+            return classTypeSymbol;
         }
     }
 }
