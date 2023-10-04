b0b17ea0134f626b5733a9ec17c6252e08717a83
unknown <yrezehi@gmail.com>
2023-10-04 14:09:58 -0700

    setup: enhance global error handler

--- Static/Exceptions/GlobalErrorHandler.cs
+++ Static/Exceptions/GlobalErrorHandler.cs
@@ -19,28 +19,23 @@ namespace Static.Exceptions
 
         private static async Task HandleException(HttpContext context, Exception exception)
         {
-            // Object holds the error details that will be logged in the server
             dynamic loggingErrorObject = new { };
 
-            // To help troubleshoot the issue, trace id will be shown to the user
             string traceId = context.TraceIdentifier.ToString();
             int statusCode = exception is HTTPException ? ((HTTPException) exception).StatusCode : HTTPException.InternalServerError;
 
-            // Show custom error thrown by the developer or Internal Error Message if unhandled error
             string? exceptionMessage = exception is HTTPException ? ((HTTPException)exception).Message : "";
 
 
-            if (exception is HTTPException)
+            if (context.Request.Path.StartsWithSegments("api"))
             {
                 loggingErrorObject = new
                 {
                     StatusCode = statusCode,
-                    Message = ((HTTPException)exception).Message,
                     Cause = "Exception was thrown by the developer's error handling error",
                     TraceId = traceId
                 };
 
-                // Convert logging error object to string and log it
                 string serializedErrorObject = JsonSerializer.Serialize(loggingErrorObject);
                 Log.Error(serializedErrorObject);
 
@@ -49,7 +44,7 @@ namespace Static.Exceptions
                 await context.Response.WriteAsync(JsonSerializer.Serialize(new
                 {
                     StatusCode = statusCode,
-                    ErrorMessage = exceptionMessage,
+                    Message = exceptionMessage,
                     TraceId = traceId
                 }));
             }
@@ -58,9 +53,6 @@ namespace Static.Exceptions
                 loggingErrorObject = new
                 {
                     StatusCode = statusCode,
-                    Message = exception.Message,
-                    StackTrace = exception.StackTrace,
-                    InnerException = exception.InnerException,
                     Cause = "Unhandled exception",
                     TraceId = traceId
                 };
