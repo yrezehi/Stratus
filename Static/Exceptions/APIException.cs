5157483a5e011dca950b1af25a454e140caabb03
unknown <yrezehi@gmail.com>
2023-10-04 13:53:37 -0700

    setup: APIException

---
+++ Static/Exceptions/APIException.cs
@@ -0,0 +1,13 @@
+﻿namespace Static.Exceptions
+{
+    public class APIException : Exception
+    {
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+
+        public APIException(int code, string message) {
+            StatusCode = code;
+            Message = message;
+        }
+    }
+}
