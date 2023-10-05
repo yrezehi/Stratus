1aa57c9018bb48295d6b12305e4c4046be2bc121
unknown <yrezehi@gmail.com>
2023-10-05 03:44:57 -0700

    add: configuration

---
+++ Static/Configuration/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Static.Configuration
+{
+    public static class Configuration
+    {
+        private static IConfiguration ConfigurationInstance = new ConfigurationBuilder().Build();
+
+        public static void Register(IConfiguration configuration) =>
+            ConfigurationInstance = configuration;
+
+        public static T GetValue<T>(string path)
+        {
+            T? value = ConfigurationInstance.GetValue<T>(path);
+
+            if(value != null)
+            {
+                throw new ArgumentException("No such configuration property path {path}!");
+            }
+
+            return value!;
+        }
+
+    }
+}
