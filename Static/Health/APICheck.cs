b996c5938d757e9f187d944db6af2cd964350296
unknown <yrezehi@gmail.com>
2023-10-05 03:00:01 -0700

    setup: APICheck

---
+++ Static/Health/APICheck.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Static.Health
+{
+    public class APICheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task<bool> IsReachable()
+        {
+            throw new NotImplementedException ();
+        }
+        private async Task<bool> IsHeadRequestReachable()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
