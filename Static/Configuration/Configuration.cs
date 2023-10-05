c0b3e9316a7e9e0ab1744eb4df7e8d8219f637e0
unknown <yrezehi@gmail.com>
2023-10-05 03:49:21 -0700

    add: GetList

--- Static/Configuration/Configuration.cs
+++ Static/Configuration/Configuration.cs
@@ -7,17 +7,34 @@
         public static void Register(IConfiguration configuration) =>
             ConfigurationInstance = configuration;
 
+        public static IConfigurationSection GetSection(string section) =>
+            ConfigurationInstance.GetSection(section);
+
         public static T GetValue<T>(string path)
         {
             T? value = ConfigurationInstance.GetValue<T>(path);
 
-            if(value != null)
+            if(value == null)
             {
                 throw new ArgumentException("No such configuration property path {path}!");
             }
 
             return value!;
         }
+
+        public static IList<T> GetList<T>(string path)
+        {
+            List<T>? value = GetSection(path).Get<List<T>>();
+
+            if (value == null || value.Count == 0)
+            {
+                throw new ArgumentException("No such configuration property path {path}!");
+            }
+
+            return value!;
+        }
+
+
 
     }
 }
