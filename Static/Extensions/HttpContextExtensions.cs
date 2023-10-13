36f025869b981db6d9e2a201b8cc0eaff9f15836
unknown <yrezehi@gmail.com>
2023-10-13 05:54:52 -0700

    add: HttpContextExtensions

---
+++ Static/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Security.Claims;
+
+namespace Static.Extensions
+{
+    public static class HttpContextExtensions
+    {
+        private static string FORWARDER_FOR_HEADER = "X-Forwarder-For";
+
+        public static string? GetIP(this HttpContext context)
+        {
+            IPAddress? ipAddress = context.Connection.RemoteIpAddress;
+
+            if (ipAddress != null)
+            {
+                return ipAddress.ToString();
+            }
+            else if (context.Request.Headers.ContainsKey(FORWARDER_FOR_HEADER))
+            {
+                return context.GetForwarderForAddressIP();
+            }
+
+            return null;
+        }
+
+        private static string? GetForwarderForAddressIP(this HttpContext context) =>
+            context!.Request.Headers.TryGetValue("X-Forwarder-For", out var ips)
+                    && IPAddress.TryParse(ips.FirstOrDefault()?.Split(',', StringSplitOptions.RemoveEmptyEntries).FirstOrDefault(), out IPAddress? clientIp)
+                        ? (clientIp.ToString()) : null;
+
+        public static string? GetCalimValue(this HttpContext context, string type) =>
+            context.User.FindFirstValue(type);
+
+        public static string? GetCookieValue(this HttpContext context, string key) =>
+            context.Request.Cookies.FirstOrDefault(cookie => cookie.Key.Contains(key)).Value;
+
+    }
+}
