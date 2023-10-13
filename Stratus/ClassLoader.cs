a18a1680a10599dbed2b75d0ce5ab5b06b91638b
unknown <yrezehi@gmail.com>
2023-10-13 11:20:22 -0700

    setup: ClassLoader

---
+++ Stratus/ClassLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace Stratus
+{
+    public class ClassLoader
+    {
+        public static void Load(string classPath) {
+
+            if (!File.Exists(classPath))
+            {
+                throw new FileNotFoundException($"Class was not found for {classPath}");
+            }
+        }
+
+        private static string LoadAsText(string classPath)
+        {
+            using (var streamReader = new StreamReader(classPath))
+            {
+                return streamReader.ReadToEnd();
+            }
+        }
+    }
+}
