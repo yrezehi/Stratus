af7addecdbf4634e28fdec7bc3ffb587844f6b00
unknown <yrezehi@gmail.com>
2023-10-13 04:37:24 -0700

    finish: grunt

---
+++ Static/Gruntfile.js
@@ -0,0 +1,21 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        jshint: {
+            files: ['wwwroot/js/*.js'],
+            options: {
+                '-W069': false,
+            }
+        },
+        uglify: {
+            all: {
+                src: ['wwwroot/js/*.js'],
+                dest: 'wwwroot/lib/minified.min.js'
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask("all", ['jshint', 'uglify']);
+};
