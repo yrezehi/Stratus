e9e484159a74958cd501062e51853adf05c9ba42
unknown <yrezehi@gmail.com>
2023-11-10 07:34:25 -0800

    add: ef interceptor

---
+++ Static/Configuration/Interceptors/LoggingInterceptor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using System.Data.Entity.Infrastructure.Interception;
+
+namespace Static.Configuration.Interceptors
+{
+    public class LoggingInterceptor : DbCommandInterceptor
+    {
+        public override void ReaderExecuted(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext)
+        {
+            Log.Info(String.Format(command.CommandText));
+        }
+    }
+}
